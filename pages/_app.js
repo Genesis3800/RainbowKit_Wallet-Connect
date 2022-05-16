43dc26bc409531b325bd658e74a1da8dda06722f
Priyank Gupta <Genesis3800>
2022-05-16 17:37:59 +0530

    Rainbow Kit commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme, midnightTheme,
+} from '@rainbow-me/rainbowkit';
+import { chain, createClient, WagmiProvider } from 'wagmi';
+
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.polygonMumbai],
+  [
+    apiProvider.alchemy(process.env.ALCHEMY_ID),
+    apiProvider.fallback()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains} coolMode>
+      <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
+  
 }
 
 export default MyApp
